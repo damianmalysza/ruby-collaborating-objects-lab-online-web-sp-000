efbafa91401257a4422a5383fc8673133d206a98
Damian Malysza <damian.malysza@gmail.com>
2020-11-01 23:16:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,8 +17,9 @@ class Song
   def self.new_by_filename(filename)
     song_name = filename.split(" - ")[1]
     artist_name = filename.split(" - ")[0]
-    self.new(song_name).artist_name = (artist_name)
-    self
+    new_song = self.new(song_name)
+    new_song.artist_name = (artist_name)
+    new_song
   end
 
   def artist_name=(artist_name)
